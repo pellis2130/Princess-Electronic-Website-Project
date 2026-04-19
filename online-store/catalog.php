6570ab2a454fff920a4df51029c240366a13c90f
pellis2130 <pellis2130@gmail.com>
2026-04-19 16:45:47 -0400

    Website Project

---
+++ online-store/catalog.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php'; ?>
+
+<h2>Product Catalog</h2>
+<p>Catalog page under construction...</p>
+
+<a href="cart.php">Go to Cart</a>
+
+<?php include 'includes/footer.php'; ?>
---
+++ online-store/includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Princess's Electronic Website</title>
+</head>
+<body>
+
+<h1>Princess Electronic Website</h1>
+<hr>
